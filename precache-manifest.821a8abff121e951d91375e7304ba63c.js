8ff5f5e4adc5918620bf105de9be1571195e00bc
Taha Waqar <syedtaha.hsn@gmail.com>
2022-09-18 13:26:06 +0500

    Updates

---
+++ precache-manifest.821a8abff121e951d91375e7304ba63c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23a960f210ee39412459",
+    "url": "/textutils/static/css/main.cc553aed.chunk.css"
+  },
+  {
+    "revision": "23a960f210ee39412459",
+    "url": "/textutils/static/js/main.23a960f2.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "ca5d6ee351c6e8ed4d589fcb2e29fc78",
+    "url": "/textutils/index.html"
+  }
+];
